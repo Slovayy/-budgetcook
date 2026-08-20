70f4199af90cc5e3577e099d7fa196e806b00e93
Slovayy <jgvwzh6vpf@privaterelay.appleid.com>
2026-08-20 11:22:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,59 @@
-const CACHE = "budgetcook-v3";
+const CACHE_NAME = "budgetcook-v4";
 
-const ASSETS = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./style.css",
+  "./data.js",
+  "./app.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
   );
+
+  self.skipWaiting();
+
 });
+
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+
+      return Promise.all(
         keys
-          .filter(key => key !== CACHE)
+          .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
-  );
-});
+      );
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-
-          caches.open(CACHE).then(cache => {
-            cache.put(event.request, copy);
-          });
-
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
     })
   );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response ||
+          fetch(event.request);
+
+      })
+
+  );
+
 });
